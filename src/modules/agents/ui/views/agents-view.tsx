04f16ddb77a259592c54091e63c107b40559266d
Anuragh <princeanuragh1@gmail.com>
2025-08-15 21:41:53 -0500

    Update agents-view.tsx

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -14,8 +14,8 @@ const AgentsView = () => {
     <div className=" flex-1 flex flex-col pb-4 px-4 md:px-8 gap-y-4">
       {data.length === 0 ? (
         <EmptyState
-          title="Create your first agent"
-          description="Create an agent to join your meetings. each agent will follow your instructions and can interact with participants during the call."
+          title=" Create your first agent"
+          description=" Create an agent to join your meetings. each agent will follow your instructions and can interact with participants during the call."
         />
       ) : (
         <DataTable data={data} columns={columns} />
